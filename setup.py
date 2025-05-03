26ec299bdc4f7399b4dad60f3db0f34087a30847
Mathi Ramilo <mathiramilo2290@gmail.com>
2025-05-02 23:09:10 -0300

    chore(release): bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="notion-watchlist",
-    version="0.1.1",
+    version="0.2.0",
     description="CLI tool for automating your movie & TV watchlist in Notion via TMDB and IMDb",
     author="Mathias Ramilo",
     author_email="mathiramilo2290@gmail.com",
